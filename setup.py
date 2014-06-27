556265cfcc58beee87f1489a21e00e6b1a3a9565
Philip Sahli <philip@sahli.net>
2014-06-27 06:16:20 +0200

    pin spyne version

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name='testws',
       version='0.1b',
       py_modules=['testws'],
-      install_requires=['spyne', 'lxml'],
+      install_requires=['spyne==2.10.10', 'lxml'],
 )
